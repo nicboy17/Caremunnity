65f99443e05a8d114a99c7f95ee244250cf2492a
Nick <nick@Nicks-MacBook-Pro.local>
2017-11-18 18:10:19 -0500

    11/17/18 @ 6:00pm

--- Caremunnity/app/src/main/java/com/example/nmajor/caremunnity/MainActivity.java
+++ Caremunnity/app/src/main/java/com/example/nmajor/caremunnity/MainActivity.java
@@ -6,10 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -18,8 +23,8 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
-    String server_url = "http://192.168.2.197:4000/api/friends/1";
-
+    String server_url = "http://74.15.46.198:4000/api/friends/1";
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.bn);
         textView = (TextView)findViewById(R.id.txt);
+
+        /*Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();*/
 
         button.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -37,17 +47,15 @@ public class MainActivity extends AppCompatActivity {
                    @Override
                    public void onResponse(JSONObject response) {
                         textView.setText(response.toString());
-                        requestQueue.stop();
                    }
                }, new Response.ErrorListener() {
                    @Override
                    public void onErrorResponse(VolleyError error) {
                        textView.setText("Server Error...");
                        error.printStackTrace();
-                       requestQueue.stop();
                    }
                });
-               requestQueue.add(jsonRequest);
+               Mysingleton.getInstance(getApplicationContext()).addToRequestque(jsonRequest);
            }
         });
     }
