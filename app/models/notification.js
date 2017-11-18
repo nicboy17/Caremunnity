c41c62e73ef23279129d0b9ae62e626a507fc23d
nicboy17 <nmajor@RTS.local>
2017-11-17 19:29:13 -0500

    nick 11/17/17 @ 7:30

--- app/models/notification.js
+++ app/models/notification.js
@@ -1,4 +1,14 @@
+var dateTime = require('node-datetime');
 var db = require('../../db');
+
+var weekday = new Array(7);
+weekday[0] =  "alert_sunday";
+weekday[1] = "alert_monday";
+weekday[2] = "alert_tuesday";
+weekday[3] = "alert_wednesday";
+weekday[4] = "alert_thursday";
+weekday[5] = "alert_friday";
+weekday[6] = "alert_saturday";
 
 var Notify = {  
     createNotification: function(user_id, goal_id, medication_id, title, start_day, end_day, sunday, monday, tuesday, wednesday, thursday, friday, saturday, alert, callback) {
@@ -6,16 +16,24 @@ var Notify = {
         ,[user_id, goal_id, medication_id, title, start_day, end_day, sunday, monday, tuesday, wednesday, thursday, friday, saturday, alert], callback);  
     },
     deleteNotification: function(notification_id, callback) {
-        return db.query('',[username, password], callback);  
+        return db.query('delete from notfication where notification_id = ?',[notification_id], callback);  
     },
     editNotification: function(notification_id, user_id, goal_id, medication_id, title, start_day, end_day, sunday, monday, tuesday, wednesday, thursday, friday, saturday, alert, callback) {
-        return db.query('',[username, password], callback);  
+        return db.query('update notifications set user_id = ? and goal_id = ? and medication_id = ? and title = ? and start_day = ? and end_day = ? and alert_sunday = ? and alert_monday = ? and alert_tuesday = ? and alert_wednesday = ? and alert_thursday = ? and alert_friday = ? and alert_saturday = ? and alert = ? where notification_id = ?'
+        ,[user_id, goal_id, medication_id, title, start_day, end_day, sunday, monday, tuesday, wednesday, thursday, friday, saturday, alert, notification_id], callback);  
     },
-    getFirstNotification: function(user_id, day, time, callback) {
-        return db.query('',[username, password], callback);  
+    getFirstNotification: function(notification_id, callback) {
+        return db.query('select * from notifications where notification_id = ?',[notification_id], callback);  
     },
-    getNotifications: function(callback) {
-        return db.query('',[username, password], callback);  
+    getFirstNotification: function(user_id, callback) {
+        var date = dateTime.create().format('Y-m-d');
+        var n = weekday[date.getDay()];
+        return db.query('select notification_id, title, alert from notifications where user_id = ? and start_date >= CURDATE() and end_date <= CURDATE() and ? = true LIMIT 1 ORDERBY alert',[user_id, n], callback);  
+    },
+    getNotifications: function(user_id, date, callback) {
+        var date = dateTime.create().format('Y-m-d');
+        var n = weekday[date.getDay()];
+        return db.query('select notification_id, title, alert from notifications where user_id = ? and start_date >= CURDATE() and end_date <= CURDATE() and ? = true ORDERBY alert',[username, password], callback);  
     }
 };  
 module.exports = Notify;
