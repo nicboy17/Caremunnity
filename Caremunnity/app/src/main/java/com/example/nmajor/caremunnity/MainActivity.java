2614616c1b7055ca2528b944fc46dde4806733e7
nicboy17 <nmajor@RTS.local>
2017-11-18 17:00:53 -0500

    11/18/17 @ 5:00pm

---
+++ Caremunnity/app/src/main/java/com/example/nmajor/caremunnity/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.nmajor.caremunnity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    String server_url = "http://192.168.2.197:4000/api/friends/1";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.bn);
+        textView = (TextView)findViewById(R.id.txt);
+
+        button.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+               final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+               JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, server_url, new Response.Listener<JSONObject>() {
+                   @Override
+                   public void onResponse(JSONObject response) {
+                        textView.setText(response.toString());
+                        requestQueue.stop();
+                   }
+               }, new Response.ErrorListener() {
+                   @Override
+                   public void onErrorResponse(VolleyError error) {
+                       textView.setText("Server Error...");
+                       error.printStackTrace();
+                       requestQueue.stop();
+                   }
+               });
+               requestQueue.add(jsonRequest);
+           }
+        });
+    }
+}
